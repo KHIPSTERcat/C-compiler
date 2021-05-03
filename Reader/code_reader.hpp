f8288a779c7f024b2a6652494f2ce837b207ced2
KHIPSTERcat <cheshko.ilia@gmail.com>
2021-05-03 20:20:47 +1000

    Reader initialization

---
+++ Reader/code_reader.hpp
@@ -0,0 +1,32 @@
+//
+// Created by chesh on 03.05.2021.
+//
+
+
+#ifndef C_COMPILER_CODE_READER_HPP
+#define C_COMPILER_CODE_READER_HPP
+
+
+#include <iostream>
+#include <string>
+
+namespace compiler{
+
+
+class CodeReader{
+ private:
+
+ public:
+  void start(){
+   std::cout << "Hi";
+ }
+
+
+};
+
+
+}
+
+
+#endif
+
---
+++ Reader/test/ReaderTest.cpp
@@ -0,0 +1,12 @@
+//
+// Created by chesh on 03.05.2021.
+//
+
+#include "code_reader.hpp"
+
+int main(){
+  compiler::CodeReader a;
+  a.start();
+
+  return 0;
+}
